b3d315b6dc15d26fee31d9e9e9aec2406b8a460f
TWMAS <93318670+TWMAS@users.noreply.github.com>
2021-11-29 10:07:54 +0100

    Add files via upload

---
+++ potencies4.sh
@@ -0,0 +1,13 @@
+for i in 0 1 2 3 4 5 6 7 8 9 
+do
+#printf "$i:"
+
+    for j in 0 1 2 3 4 5 6 7 8 9 
+        do
+        #echo "$i $j"
+        printf "($i,$j) "
+    done
+
+    echo ""
+    
+done
